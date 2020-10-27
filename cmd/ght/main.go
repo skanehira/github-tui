bd40e30261b075b2fc0624921af006bb29bd2281
skanehira <sho19921005@gmail.com>
2020-10-28 07:31:46 +0900

    move main.go

--- cmd/ght/main.go
+++ cmd/ght/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/goccy/go-yaml"
+	"github.com/shurcooL/githubv4"
+	"github.com/skanehira/ght/github"
 )
 
 var Config config
@@ -43,4 +46,25 @@ func init() {
 	conf.ConfigFile = configFile
 
 	Config = conf
+}
+
+func main() {
+	github.NewClient(Config.GitHub.Token)
+
+	variables := map[string]interface{}{
+		"login":  githubv4.String("skanehira"),
+		"first":  githubv4.Int(10),
+		"cursor": (*githubv4.String)(nil),
+	}
+
+	repos, err := github.GetRepos(variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(repos); err != nil {
+		log.Fatal(err)
+	}
 }
